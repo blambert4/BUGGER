e28c016db7a7730cb3d8050d789af0fe6e18a65a
brad lamb <bradley.cp.lambert@gmail.com>
2021-05-28 19:53:42 -0400

    lets see

--- server.js
+++ server.js
@@ -25,45 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(routes)
 
-/*app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, 'folder', 'index.html'));
 
-})*/
-app.post("/insert", (req,res)=>{
-    const type = req.body.type;
-    const assignee = req.body.assignee;
-    const importance = req.body.importance;
-    const comments = req.body.comments;
-    const statement = "INSERT INTO bugs (type,assignee,importance,comments) VALUES ($1,$2, $3, $4)"
-    database.query(statement, [type,assignee,importance,comments], (err,response)=>{
-        if(err){console.log(err)} else {res.send(response)}
-    })
-})
 
-app.get("/find", (req,res)=>{
-    const statement = "SELECT * FROM bugs"
-    database.query(statement, (err,response)=>{
-        if(err){console.log(err)}else{ res.send(response)}
-    })
-})
-app.delete("/delete/:id", (req,res)=>{
-    const id = req.params.id;
-    const statement = "DELETE FROM bugs WHERE id = $1"
-    database.query(statement, [id], (err, response)=>{
-        if(err){console.log(err)}else{ res.send(response)}
-    })
-})
-
-app.put("/edit/:id", (req,res)=>{
-    const id = req.params.id;
-    const type = req.body.type;
-    const assignee = req.body.assignee;
-    const importance = req.body.importance;
-    const comments = req.body.comments;
-    const statement = "UPDATE bugs SET type = $1, assignee = $2, importance = $3, comments = $4  WHERE id = $5"
-    database.query(statement, [type, assignee, importance, comments, id], (err, response)=>{
-        if(err){console.log(err)}else{ res.send(response)}
-    })
-})
 
 app.listen(port, '0.0.0.0', ()=>{console.log(`running on port ${port}`)})
