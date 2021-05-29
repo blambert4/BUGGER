3ee48f614f8318218101ed2b729684993016df5c
brad lamb <bradley.cp.lambert@gmail.com>
2021-05-29 16:02:00 -0400

    fine tuning

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -12,14 +12,12 @@ function Home(){
     }
     const move2 = (e) =>{
         const card = document.querySelector('.card2');
-        console.log("work")
         let Xaxis = (window.innerWidth / 2 - e.pageX)/20;
         let Yaxis = (window.innerHeight / 2 - e.pageY)/20;
         card.style.transform = `rotateX(${Yaxis}deg) rotateY(${Xaxis}deg)`;
     }
     const move3 = (e) =>{
         const card = document.querySelector('.card3');
-        console.log("work")
         let Xaxis = (window.innerWidth / 2 - e.pageX)/20;
         let Yaxis = (window.innerHeight / 2 - e.pageY)/20;
         card.style.transform = `rotateX(${Yaxis}deg) rotateY(${Xaxis}deg)`;
--- client2/src/components/submitbug.js
+++ client2/src/components/submitbug.js
@@ -3,6 +3,7 @@ import FormControl from 'react-bootstrap/FormControl';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
 import {useState, useEffect} from "react";
 import "../css/submitbug.css";
 import axios from "axios";
@@ -14,6 +15,7 @@ function Submit(){
     const [assignee, setAssignee] = useState("");
     const [importance, setImportance] = useState("");
     const [comments, setComments] = useState("");
+
     const success = ()=>{
       document.getElementsByTagName("h3")[0].innerHTML = "Success!"
     }
@@ -24,21 +26,70 @@ function Submit(){
 
     return(
         <>
-        <div id = "ok" className = "k">
-        <InputGroup className="mb-3">
-          <DropdownButton
-            as={InputGroup.Prepend}
-            variant="outline-secondary"
-            title="Bug Type"
-            id="input-group-dropdown-1"
-          >
-            <Dropdown.Item id = "one" href="#" onClick = {()=>{setBugType("Compilation")}}>Compilation</Dropdown.Item>
-            <Dropdown.Item id = "two" href="#" onClick = {()=>{setBugType("Run Time")}}>Run Time</Dropdown.Item>
-            <Dropdown.Item id = "three" href="#" onClick = {()=>{setBugType("Other")}}>Other (Explain)</Dropdown.Item>
-          </DropdownButton>
-          <FormControl aria-describedby="basic-addon1" value = {bugType}/>
-        </InputGroup>
 
+
+        <div id = "ok" className = "k">
+          <Carousel variant = "dark" className = "cardz" style = {{position: 'absolute', left: '13%'}}>
+            <Carousel.Item>
+            <img
+                style = {{position: 'relative', height: '60vh'}}
+                src="https://www.paifoundation.org/wp-content/uploads/2015/10/Blank-White-square-thumbnail.jpg"
+                alt="First slide"
+            />
+              <Carousel.Caption>
+                <h5 style = {{color: "black"}}>Bug Type</h5>
+                <InputGroup className="mb-3">
+                            <DropdownButton
+                              as={InputGroup.Prepend}
+                              variant="outline-secondary"
+                              title="Bug Type"
+                              id="input-group-dropdown-1"
+                            >
+                              <Dropdown.Item id = "one" href="#" onClick = {()=>{setBugType("Compilation")}}>Compilation</Dropdown.Item>
+                              <Dropdown.Item id = "two" href="#" onClick = {()=>{setBugType("Run Time")}}>Run Time</Dropdown.Item>
+                              <Dropdown.Item id = "three" href="#" onClick = {()=>{setBugType("Other")}}>Other (Explain)</Dropdown.Item>
+                            </DropdownButton>
+                            <FormControl aria-describedby="basic-addon1" value = {bugType} id = "ij" />
+                        </InputGroup>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                style = {{height: "60vh"}}
+                src="https://www.paifoundation.org/wp-content/uploads/2015/10/Blank-White-square-thumbnail.jpg"
+                alt="Second slide"
+            />
+              <Carousel.Caption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img
+                style = {{height: "60vh"}}
+                src="https://www.paifoundation.org/wp-content/uploads/2015/10/Blank-White-square-thumbnail.jpg"
+                alt="Third slide"
+            />
+              <Carousel.Caption>
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        {/*
+        <InputGroup className="mb-3">
+                  <DropdownButton
+                    as={InputGroup.Prepend}
+                    variant="outline-secondary"
+                    title="Bug Type"
+                    id="input-group-dropdown-1"
+                  >
+                    <Dropdown.Item id = "one" href="#" onClick = {()=>{setBugType("Compilation")}}>Compilation</Dropdown.Item>
+                    <Dropdown.Item id = "two" href="#" onClick = {()=>{setBugType("Run Time")}}>Run Time</Dropdown.Item>
+                    <Dropdown.Item id = "three" href="#" onClick = {()=>{setBugType("Other")}}>Other (Explain)</Dropdown.Item>
+                  </DropdownButton>
+                  <FormControl aria-describedby="basic-addon1" value = {bugType} id = "ij" />
+              </InputGroup>
         <InputGroup className="mb-3">
           <DropdownButton
             as={InputGroup.Prepend}
@@ -48,7 +99,7 @@ function Submit(){
           >
             <Dropdown.Item href="#" onClick = {()=>{setAssignee("Bradley")}}>Bradley Lambert</Dropdown.Item>
             <Dropdown.Item href="#" onClick = {()=>{setAssignee("Kahjha")}}>Kahjha Jappa</Dropdown.Item>
-            <Dropdown.Item href="#" onClick = {()=>{setAssignee("James")}}>James Charles</Dropdown.Item>
+            <Dropdown.Item href="#" onClick = {()=>{setAssignee("Brian")}}>Brian McKnight</Dropdown.Item>
             <Dropdown.Item href="#" onClick = {()=>{setAssignee("Unassigned")}}>Unassigned</Dropdown.Item>
           </DropdownButton>
           <FormControl aria-describedby="basic-addon1" value = {assignee}/>
@@ -74,9 +125,9 @@ function Submit(){
             <InputGroup.Text>Comments</InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl as="textarea" aria-label="With textarea" onChange = {(e)=>{setComments(e.target.value)}}/>
-        </InputGroup>
+    </InputGroup>
         <p> Does this look correct?<Button variant="success" onClick = {()=>{submit(); success()}}>Submit</Button></p>
-        <h3 style = {{color: 'green'}}> </h3>
+        <h3 style = {{color: 'green'}}> </h3>*/}
 </div>
 </>
     )
